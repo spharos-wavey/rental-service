94b948a9b09778c8ff2fadf92f9884f1fa2d90c4
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-05-05 12:54:58 +0900

    [Create] 예외 처리

---
+++ src/main/java/xyz/wavey/rentalservice/exception/ServiceException.java
@@ -0,0 +1,17 @@
+package xyz.wavey.rentalservice.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ServiceException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    public ServiceException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
