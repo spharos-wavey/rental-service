08dd41d878e969b13d693d89e1aa7612ab50c00a
minji <kmj99a@gmail.com>
2023-05-17 11:30:23 +0900

    [Create] RequestReturnTime vo 생성

---
+++ src/main/java/xyz/wavey/rentalservice/vo/RequestReturnTime.java
@@ -0,0 +1,14 @@
+package xyz.wavey.rentalservice.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestReturnTime {
+    private LocalDateTime returnTime;
+}
