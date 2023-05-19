009d7bb8c0fb2329be06adef5cf1f5d117a730fa
minji <kmj99a@gmail.com>
2023-05-19 10:35:04 +0900

    [Create] Rental Jpa 쿼리 추가

--- src/main/java/xyz/wavey/rentalservice/repository/RentalRepo.java
+++ src/main/java/xyz/wavey/rentalservice/repository/RentalRepo.java
@@ -1,8 +1,15 @@
 package xyz.wavey.rentalservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import xyz.wavey.rentalservice.model.Rental;
+import java.util.List;
 
 public interface RentalRepo extends JpaRepository<Rental, Long> {
 
+    @Query(value = "select * from rental where uuid = :uuid and purchase_state = :purchaseState", nativeQuery = true)
+    List<Rental> findAllByUuidAndPurchaseState(@Param("uuid") String uuid, @Param("purchaseState") String purchaseState);
+
+    Rental findByUuidAndId(String uuid, Long id);
 }
