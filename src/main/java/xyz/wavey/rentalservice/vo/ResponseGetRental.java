f7ab12e47dc6e6d0377e3ec1f2f2d5d24c995aba
minji <kmj99a@gmail.com>
2023-05-19 10:53:40 +0900

    [Update] 이용 내역 상세 조회 vo 수정

--- src/main/java/xyz/wavey/rentalservice/vo/ResponseGetRental.java
+++ src/main/java/xyz/wavey/rentalservice/vo/ResponseGetRental.java
@@ -1,18 +1,25 @@
 package xyz.wavey.rentalservice.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseGetRental {
 
-    public Long rentalId;
-    public Long vehicleId;
-    public String startTime;
-    public String endTime;
-    public String totalRentTime;
-    public String insuranceName;
-    public Integer insurancePrice;
+    private Long rentalId;
+    private Long vehicleId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer billitaZoneId;
+    private Integer price;
+    private Integer insuranceId;
+    private String payment;
 
 }
