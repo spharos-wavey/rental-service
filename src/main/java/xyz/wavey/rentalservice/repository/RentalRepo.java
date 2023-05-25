9fa7738b593b45ec2d51d9f51311eac2caeb1927
Ji-Wook, Kim <20korea01@gmail.com>
2023-05-25 11:55:56 +0900

    [Create] 대여 시 유효성 체크

--- src/main/java/xyz/wavey/rentalservice/repository/RentalRepo.java
+++ src/main/java/xyz/wavey/rentalservice/repository/RentalRepo.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import xyz.wavey.rentalservice.model.Rental;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +17,8 @@ public interface RentalRepo extends JpaRepository<Rental, Long> {
     List<Rental> findAllByUuid(String uuid);
 
     Optional<Rental> findByIdAndUuid(Long id, String uuid);
+
+    @Query(value = "select r from Rental as r where r.uuid = :uuid and r.endDate > :startDate and r.startDate < :endDate")
+    List<Rental> checkUserCanBool(@Param("uuid") String uuid, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
 }
